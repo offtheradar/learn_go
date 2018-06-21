57486773ee2e06a6db912324c0e01d570d8a567d
Yacine Sibous <yacinesibous@gmail.com>
2018-06-21 17:47:51 -0400

    playing with fns, and var declarations

--- main.go
+++ main.go
@@ -6,10 +6,25 @@ import(
 
 func	main(){
 	name, power := "Yacine", getPower()
-	power = 9000
-	fmt.Printf("It's over %s, %d\n", name, power)
+	power = add(power, 9000)
+	_, bob := do_some(name)
+	if !bob {
+		fmt.Printf("It's over %s, %d\n", name, power)
+	}
 }
 
 func	getPower() int {
 	return 9001
+}
+
+func	add(a int, b int) int {
+	return (a + b)
+}
+
+func	do_some(name string) (int, bool) {
+	if name == "pedro" {
+		return 5, true
+	} else {
+		return 10, false
+	}
 }
